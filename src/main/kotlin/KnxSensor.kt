4e8a876463c05a1db8a91c0e47a56c9a050da7ed
Mathias Hertlein <mathias@mh.cx>
2021-01-24 23:25:01 +0100

    Remove unused stuff

--- src/main/kotlin/KnxSensor.kt
+++
@@ -1,8 +0,0 @@
-import knx.DPT
-import knx.GroupAddress
-
-class KnxSensor(
-    val name: String,
-    val address: GroupAddress,
-    val type: DPT,
-)
--- src/main/kotlin/KnxSensors.kt
+++
@@ -1,8 +0,0 @@
-import knx.GroupAddress
-import knx.GroupAddresses
-
-interface KnxSensors : Collection<KnxSensor> {
-    fun groupAddresses(): GroupAddresses
-
-    fun sensorForGroupAddress(address: GroupAddress): KnxSensor
-}
