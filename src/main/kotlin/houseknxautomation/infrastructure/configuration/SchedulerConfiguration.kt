b622801f28f8e2439d0c0d95baebd0c93cc88fc7
Mathias Hertlein <mathias@mh.cx>
2025-06-05 22:06:39 +0200

    Exit on error

---
+++ src/main/kotlin/houseknxautomation/infrastructure/configuration/SchedulerConfiguration.kt
@@ -0,0 +1,17 @@
+package houseknxautomation.infrastructure.configuration
+
+import houseknxautomation.infrastructure.tasks.TasksErrorHandler
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.TaskScheduler
+import org.springframework.scheduling.concurrent.SimpleAsyncTaskScheduler
+
+@Configuration
+class SchedulerConfiguration(private val errorHandler: TasksErrorHandler) {
+    @Bean
+    fun taskScheduler(): TaskScheduler {
+        val taskScheduler = SimpleAsyncTaskScheduler()
+        taskScheduler.setErrorHandler(errorHandler)
+        return taskScheduler
+    }
+}
---
+++ src/main/kotlin/houseknxautomation/infrastructure/tasks/TasksErrorHandler.kt
@@ -0,0 +1,14 @@
+package houseknxautomation.infrastructure.tasks
+
+import kotlin.system.exitProcess
+import org.springframework.boot.SpringApplication
+import org.springframework.context.ApplicationContext
+import org.springframework.stereotype.Component
+import org.springframework.util.ErrorHandler
+
+@Component
+class TasksErrorHandler(private val context: ApplicationContext) : ErrorHandler {
+    override fun handleError(t: Throwable) {
+        exitProcess(SpringApplication.exit(context))
+    }
+}
